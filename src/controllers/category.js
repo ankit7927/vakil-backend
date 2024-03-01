deaf4b92fc938cd452c3dfd602c4db678563232d
Ankit Prajapati <ap8424856@gmail.com>
2024-03-01 15:43:46 +0530

    created category controllers

---
+++ src/controllers/category.js
@@ -0,0 +1,30 @@
+const category = require("../models/case/category");
+
+
+const categoryController = {};
+
+categoryController.createCategory = async (req, res, next) => {
+    const data = req.body;
+    try {
+        const newCategory = await category.create(data);
+        if (newCategory) res.json(newCategory);
+        else res.status(500).json({ message: "something went wrong" });
+    } catch (error) {
+        next(error)
+    }
+}
+
+categoryController.updateCategory = async (req, res, next) => {
+    const data = req.body;
+    if (!data._id) return res.status(404).json({ message: "id is required" });
+
+    const existing = await category.findOneById({ _id: data._id });
+    if (!existing) res.status(404).json({ message: "category not found" });
+
+    existing.name = data.name;
+
+    await existing.save();
+    res.json({ message: "category updated" });
+}
+
+module.exports = categoryController;
